e590b13d7e21df38ba9564a57949eca6e41081ef
darksnail <darksnail@gmail.com>
2018-08-28 12:13:26 -0400

    Added function that fits neurnon receptive fields by least squares

--- src/sub_fit_rf_centers.jl
+++ src/sub_fit_rf_centers.jl
@@ -1,11 +1,65 @@
 
 module FitRFCenters
-using Distributions, JuMP
+using Distributions, NLopt, ForwardDiff
 
 
 function response_distribution(x,y,offset,xc,yc,gain,width)
     sqr_d = (x-xc)^2 + (y - yc)^2
     offset + gain * exp(-sqr_d/width) # meh, forget normalization
+end
+
+function square_error(xs,ys,zs, offset,xc,yc,gain,width)
+    rd(x,y) = response_distribution(x,y,offset,xc,yc,gain,width)
+    out = broadcast( (x,y,z) -> (z - rd(x,y))^2 , xs,ys,zs)
+    mean(out)
+end
+
+function get_cost_fun(x_data,y_data,z_data)
+    function _cost(p::Vector)
+        offset,xc,yc,gain,width = p
+        square_error(x_data,y_data,z_data, offset,xc,yc,gain,width)
+    end
+    function _cost(p::Vector, g::Vector)
+        obj=_cost(p)
+        if length(g) > 0
+            ForwardDiff.gradient!(g,_cost,p)
+        end
+        obj
+    end
+    _cost
+end
+
+function fit_receptive_field(x_data,y_data,z_data,guess, min_width::Float64)
+
+    objective_fun = get_cost_fun(x_data,y_data,z_data)
+    # minimum width
+    function constraint1(p::Vector,g::Vector)
+        width=p[5]
+        cst = min_width - p
+        if length(g) > 0
+            g .= [0,0,0,0,-1.]
+        end
+        cst
+    end
+    #positive offset
+    function constraint2(p::Vector,g::Vector)
+        offset=p[1]
+        cst = 0.001 - offset
+        if length(g) > 0
+            g .= [-1.,0,0,0,0.]
+        end
+        cst
+    end
+
+    opt = Opt(:LD_MMA,5)
+    lower_bounds!(opt,[0.001, -Inf, -Inf, 0.0 , min_width])
+    xtol_rel!(opt,1E-4)
+
+    min_objective!(opt,objective_fun)
+    # inequality_constraint!(opt,constraint1,1E-8)
+    # inequality_constraint!(opt,constraint2,1E-8)
+
+    minf,minx,ret = optimize(opt,guess)
 end
 
 
--- tests/fitrfc.jl
+++ tests/fitrfc.jl
@@ -5,18 +5,36 @@ const F = GsmModules.FitRFCenters
 using Statistics, StatsBase , LinearAlgebra
 using Random, Distributions
 
-using Plots
 ##
-_ = let
-    xs = LinRange(0,2,200)
-    ys = LinRange(0,-2,200)
-    xc = 1.
-    yc = -1.
-    gain=2.
-    offset = 0.5
-    width = 0.2
-    zs = broadcast(F.response_distribution,xs,ys',offset,xc,yc,gain,width)
-    @show extrema(zs)
-    surface(xs,ys,zs)
+# make some data, make sure the cost is small for ground truth
+xc = 1.
+yc = -1.
+gain=2.
+offset = 0.5
+width = 0.1
+
+xs,ys,zs = let
+    xsg = LinRange(0,2,200)
+    ysg = LinRange(0,-2,200)
+    _ys = rand(ysg,200)
+    _xs = rand(xsg,200)
+    _zs = F.response_distribution.(_xs,_ys,offset,xc,yc,gain,width)
+    (_xs,_ys,_zs)
 end
-    
+
+_cost_test = F.get_cost_fun(xs,ys,zs)
+
+_grad_test = fill(NaN,5)
+_cost_test([0.5, 1,-1,2.,0.1 ] , _grad_test)
+
+_grad_test
+
+##
+using Plots
+scatter(xs,ys,zs)
+
+##
+# fit it!
+
+guess = [0.5, 1,-1,2.,0.3 ]
+whatevs = F.fit_receptive_field(xs,ys,zs, guess, 0.01 )
